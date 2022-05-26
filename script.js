1dc84ea67e785b636a67aa138765035f10e05f22
cGuilhermec <104534469+cGuilhermec@users.noreply.github.com>
2022-05-25 21:28:52 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function verificar() {
-    
+function Ver() {
+    var radios = window.document.getElementsByName('radtim')
+    var res = window.document.getElementById('res')
+    if(radios[0].checked) {
+        res.innerHTML = 'Palmeiras'
+    } else if(radios[1].checked) {
+        res.innerHTML = 'Corinthians'
+    } else if(radios[2].checked) {
+        res.innerHTML = 'São Paulo'
+    } else if(radios[3].checked) {
+        res.innerHTML = 'Santos'
+    }
 }
